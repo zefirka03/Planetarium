b81e89f9c6ab403e8ace473a4ab9c6213be82624
zefirka03 <newpontos@gmail.com>
2024-01-03 16:36:37 +0300

    fix

--- core/AirCore.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "AirUtils.h"
-#include "Game.h"
-#include "VBO.h"
-#include "VAO.h"
-#include "ECS.h"
-#include "Scene.h"
-#include "Shader.h"
--- core/AirUtils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <string>
-
-#define AIR_NAMESPACE_BEGIN namespace air {
-#define AIR_NAMESPACE_END }
-
-#define AIR_LOG(f) printf((std::string("[AIR]: ") + f + "\n").c_str());
-#define AIR_LOG(f, ...) printf((std::string("[AIR]: ") + f + "\n").c_str(), __VA_ARGS__);
